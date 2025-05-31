d6fe01c5606453ee17ba568d4c5d03900db61edf
afrozasalman <tech.afroza@gmail.com>
2025-05-31 22:36:16 +0600

    scroll effects

--- sample_scrape.py
+++ sample_scrape.py
@@ -15,6 +15,17 @@ driver.maximize_window()
 title = driver.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div/div[2]/div[2]/a').text
 
 link = driver.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div/div[2]/div[2]/a').get_attribute('href')
+img  = driver.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div/div[1]/div/a/div/img').get_attribute('src')
+
+import requests
+
+response = requests.get(img)
+
+print(response.content)
+print(type(response))
+
+with open('images/downloaded_image.jpg', 'wb') as f:
+    f.write(response.content)
 
 print(title,link)
 
